404a1d5920bdac0e8faaf6649094eb5dff3bef36
Emily Wells Jimenez and James Blocker <emily+james@>
2017-09-20 10:00:07 -0700

    add user front end and start of leap year function

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var leapYear = function(year) {
+  return false;
+};
+
+// jQuery UI logic is below
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+  });
+});
